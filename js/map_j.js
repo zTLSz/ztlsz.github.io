9dafb24ec3fc1bc060a6536e27e745b55ada14d3
zTLSz <ararador447@yandex.ru>
2018-06-21 23:00:38 +0300

    j

---
+++ js/map_j.js
@@ -0,0 +1,157 @@
+
+// map
+
+      function initMap() {
+        // Styles a map in night mode.
+        var mark1 = {lat: 56.306, lng: 44.029};
+        var mark2 = {lat: 56.282, lng: 43.931};
+
+        
+        var map2 = new google.maps.Map(document.getElementById('map2'), {
+          center: {lat: 56.284, lng: 43.997},
+          zoom: 12,
+          styles: [
+    {
+        "featureType": "administrative",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": "-100"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.province",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 65
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": "50"
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": "-100"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "all",
+        "stylers": [
+            {
+                "lightness": "30"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "all",
+        "stylers": [
+            {
+                "lightness": "40"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "hue": "#ffff00"
+            },
+            {
+                "lightness": -25
+            },
+            {
+                "saturation": -97
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "lightness": -25
+            },
+            {
+                "saturation": -100
+            }
+        ]
+    }
+]
+        });
+
+
+
+        var marker = new google.maps.Marker({
+          position: mark1,
+          map: map2,
+          icon: 'img/icons/geo.svg'
+        });
+
+        var marker2 = new google.maps.Marker({
+          position: mark2,
+          map: map2,
+          icon: 'img/icons/geo.svg'
+        });
+
+      }
+
+
+            
